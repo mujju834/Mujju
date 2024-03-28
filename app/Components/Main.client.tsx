20da921125505d2492214e1d75fca1285a948140
MUJAHID <mohammadmujahid383@gmail.com>
2024-03-28 03:50:59 -0400

    committed titles issue

--- app/Components/Main.client.tsx
+++ app/Components/Main.client.tsx
@@ -4,10 +4,10 @@ import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
 
+const titles = ['Blockchain Developer', 'Full Stack Maestro', 'Cloud Architect', 'DevOps Specialist', 'Networking Enthusiast'];
 
 const Main = () => {
 
-  const titles = ['Blockchain Developer', 'Full Stack Maestro', 'Cloud Architect', 'DevOps Specialist', 'Networking Enthusiast'];
   const [currentTitle, setCurrentTitle] = useState(titles[0]);
   const [fade, setFade] = useState(true);
 
