4cc49cdffaf0cddf972709cd91634255b4cae2e6
MOHAMMAD MUJAHID <72245261+mujju834@users.noreply.github.com>
2024-09-04 03:14:00 -0400

    added a new project

--- app/Components/Projects.tsx
+++ app/Components/Projects.tsx
@@ -17,6 +17,15 @@ const projectsData = [
   "liveUrl": "https://mujju-search-engine.onrender.com/", // Replace with the actual live project URL
   "codeUrl": "https://github.com/mujju834/Mujju-search-engine" // Replace with the actual GitHub repo
 },
+{
+  "name": "Mujju's WhatsApp Clone",
+  "description": "Mujju's WhatsApp Clone is a real-time messaging and calling application that allows users to chat and make audio calls. Users can register with their phone numbers, send messages, and make secure peer-to-peer audio calls, just like popular messaging apps. The app leverages modern web technologies and Socket.io for real-time interactions.",
+  "tech": ["Node.js", "Express", "React", "Socket.io", "SimplePeer", "MongoDB", "Bootstrap", "Twilio API", "JavaScript", "HTML", "CSS"],
+  "image": "/images/Mujju-whatsapp.png", // Replace with the actual image path
+  "liveUrl": "https://my-own-whatsapp.onrender.com/", // Replace with the actual live project URL
+  "codeUrl": "https://github.com/mujju834/My-own-whatsapp.git" // Replace with the actual GitHub repo
+},
+
 
 {
   "name": "Mujju's Ethereum Wallet",
